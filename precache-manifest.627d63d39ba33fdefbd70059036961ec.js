d16c4c96be56b2265c4c9fd1dc830133c9351b10
Gayathri Balasubramani <gayathri.balu24@gmail.com>
2020-10-09 23:01:05 +0100

    Updates

---
+++ precache-manifest.627d63d39ba33fdefbd70059036961ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8c7b8f2b90bd6bccb943ccab65d013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec4719b873a07b3a98da",
+    "url": "/static/css/main.3aaced87.chunk.css"
+  },
+  {
+    "revision": "931b35bba5798ce93b3b",
+    "url": "/static/js/2.b70395ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b70395ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4719b873a07b3a98da",
+    "url": "/static/js/main.63e557ea.chunk.js"
+  },
+  {
+    "revision": "4636e19e4ced5e4bd0ea",
+    "url": "/static/js/runtime-main.030ebcf0.js"
+  }
+]);
